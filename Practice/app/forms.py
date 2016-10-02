38f1bf2e27811b5b65847786bee5b9877e0d6e0a
Jake Holland <sc15j3h@leeds.ac.uk>
2016-10-02 17:47:50 +0100

    Its Now A WORKING CALCULATOR

--- Practice/app/forms.py
+++ Practice/app/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+class CalculatorForm(Form):
+    number1 = IntegerField ('number1', validators= [DataRequired()])
+    number2 = IntegerField ('number2', validators=[DataRequired()])
--- Practice/app/views.py
+++ Practice/app/views.py
@@ -1,35 +1,17 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import LoginForm
+from .forms import CalculatorForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}
-    posts = [
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
+    return 'Hello Jake!'
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    form = CalculatorForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+        flash('Succesfully received form data. %s + %s  = %s'%(form.number1.data, form.number2.data, form.number1.data+form.number2.data))
+    return render_template('calculator.html',
+                           title='Calculator',
+                           form=form)
