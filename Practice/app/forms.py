bbcf6272344536f248b5121587288bc734bc12c9
Jake Holland <sc15j3h@leeds.ac.uk>
2016-10-02 16:58:14 +0100

    Starting Again

--- Practice/app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, BooleanField, IntegerField
-from wtforms.validators import DataRequired
-
-
-class CalculatorForm(Form):
-    number1 = IntegerField('number1', validators=[DataRequired()])
-    number2 = IntegerField('number2', validators=[DataRequired()])
