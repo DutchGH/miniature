263cee7ff50bd0cbf56a9381539ca1a98ce64ca2
Jake Holland <sc15j3h@leeds.ac.uk>
2016-10-06 21:57:08 +0100

    Sort of working

--- CW1/app/models.py
+++ CW1/app/models.py
@@ -3,12 +3,13 @@ from app import db
 class ToDo(db.Model):
     __tablename__ = "todo"
     id = db.Column('id', db.Integer, primary_key=True)
-    priority_id = db.Column('priority_id', db.Integer, db.ForeignKey('category.id'))
+    priority_id = db.Column('priority_id', db.Integer, db.ForeignKey('priority.id'))
     description = db.Column('description', db.Unicode)
     creation_date = db.Column('creation_date', db.Date)
-    is_done = db.Column('is_done', db.Boolean)
+    is_done = db.Column('is_done', db.Boolean, default= False)
 
-    priority = db.relationship('Priority', foreign_keys = priority_id)
+    priority = db.relationship('Priority', foreign_keys = priority_id,
+                               backref = "todos")
 
 
 class Priority(db.Model):
@@ -16,3 +17,5 @@ class Priority(db.Model):
     id = db.Column('id', db.Integer, primary_key= True)
     name = db.Column('name', db.Unicode)
     value = db.Column('value', db.Integer)
+
+
--- CW1/app/views.py
+++ CW1/app/views.py
@@ -1,11 +1,17 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
+from .models import *
 from app import app
-from .forms import CalculatorForm
+from .forms import *
+
+
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title = 'Home Page')
+def list_all():
+    return render_template(
+        'list.html',
+        todos=ToDo.query.all() #primaryJoin(Priority).order_by(Priority.value.desc())
+    )
+
 
 @app.route('/calculator', methods=['GET', 'POST'])
 def calculator():
@@ -16,9 +22,21 @@ def calculator():
                            title='Calculator',
                            form=form)
 
-@app.route('/newtask', methods=['GET','POST'])
-def newTask():
-    return 'Create a New Task Here'
+@app.route('/newTask', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        priority = Priority.query.filter_by(id=request.form['priority']).first()
+        todo = ToDo(priority=priority, description=request.form['description'])
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template(
+            'new-task.html',
+            page='new-task',
+            priorities=Priority.query.all()
+        )
+
 
 @app.route('/viewtask')
 def viewTask():
