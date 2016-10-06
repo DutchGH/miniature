3abd245c57b8e6215c77c2bec252646c3042baf2
Jake Holland <sc15j3h@leeds.ac.uk>
2016-10-06 13:14:11 +0100

    Refined Database Model to include priority mode

--- CW1/app/models.py
+++ CW1/app/models.py
@@ -1,6 +1,18 @@
 from app import db
 
-class toDoList(db.Model):
-    id = db.Column (db.Integer, primary_key =  True)
-    taskDesc = db.Column (db.String[500], index = True)
-    taskComplete = db.Column (db.Boolean)
+class ToDo(db.Model):
+    __tablename__ = "todo"
+    id = db.Column('id', db.Integer, primary_key=True)
+    priority_id = db.Column('priority_id', db.Integer, db.ForeignKey('category.id'))
+    description = db.Column('description', db.Unicode)
+    creation_date = db.Column('creation_date', db.Date)
+    is_done = db.Column('is_done', db.Boolean)
+
+    priority = db.relationship('Priority', foreign_keys = priority_id)
+
+
+class Priority(db.Model):
+    __tablename__ = "priority"
+    id = db.Column('id', db.Integer, primary_key= True)
+    name = db.Column('name', db.Unicode)
+    value = db.Column('value', db.Integer)
