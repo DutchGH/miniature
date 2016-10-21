2aa3269363bbd61ecf9e4dad9dedaa1ffd145b8a
Jake Holland <sc15j3h@leeds.ac.uk>
2016-10-21 22:54:05 +0100

    Complete, must refine entry to be simpler

--- CW1/app/forms.py
+++ CW1/app/forms.py
@@ -2,11 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, InputRequired
 
-class CalculatorForm(FlaskForm):
-    number1 = IntegerField ('number1', validators= [InputRequired()])
-    number2 = IntegerField ('number2', validators=[InputRequired()])
-    number3 = IntegerField ('number3', validators=[InputRequired()])
 
 class ToDoList(FlaskForm):
-    description =  StringField ('description', validators= [InputRequired()])
-    is_done = BooleanField ('is_done')
+	name = StringField ('name', validators = [InputRequired()])
+	description = StringField ('description', validators= [InputRequired()])
+	is_done = BooleanField ('is_done')
--- CW1/app/views.py
+++ CW1/app/views.py
@@ -3,28 +3,18 @@ from .models import *
 from app import app
 from .forms import *
 
-
-
 @app.route('/', methods = ['GET', 'POST'])
 def list_all():
     return render_template(
         'list.html',
-        todos=ToDo.query.filter_by(is_done = False).all() #primaryJoin(Priority).order_by(Priority.value.desc())
+        todos=ToDo.query.filter_by(is_done = False).all() 
     )
 
-@app.route('/calculator', methods=['GET', 'POST'])
-def calculator():
-    form = CalculatorForm()
-    if form.validate_on_submit():
-        flash('Succesfully received form data. %s + %s + %s = %s'%(form.number1.data, form.number2.data, form.number3.data, form.number1.data+form.number2.data+form.number3.data))
-    return render_template('calculator.html',
-                           title='Calculator',
-                           form=form)
 
 @app.route('/newTask', methods=['GET', 'POST'])
 def new():
     if request.method == 'POST':
-        todo = ToDo(description=request.form['description'])
+        todo = ToDo(name=request.form['name'],description=request.form['description'])
         db.session.add(todo)
         db.session.commit()
         return redirect('/')
@@ -45,6 +35,13 @@ def editDB(id):
         x.is_done = False
         db.session.commit()
         return redirect('/')
+
+@app.route('/delete_db/<id>')
+def deleteDB(id):
+    x = ToDo.query.get(id)
+    db.session.delete(x)
+    db.session.commit()
+    return redirect('/viewcomplete')
 
 
 @app.route('/viewcomplete')
