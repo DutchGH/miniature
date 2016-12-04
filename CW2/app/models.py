62afccb9ef71dfea3f9dc552d11a51c91b623271
Jacob Holland <Jacob Holland>
2016-12-04 22:36:21 +0000

    starting login functionality

--- CW2/app/models.py
+++ CW2/app/models.py
@@ -6,20 +6,23 @@ class url_table(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     short_url = db.Column('url', db.String(100))
     long_url = db.Column(db.Text)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, short_url, long_url):
         self.short_url = short_url
         self.long_url = long_url
 
 
-# class user(db.Model):
-# 	__tablename__ = "user"
-# 	id = db.Column('id', db.Integer, primary_key = True)
-# 	user_name = db.Column(db.String(500), index = True, unique = True)
-# 	email = db.Column(db.String(500), index = True, unique = True)
-# 	password = db.Column(db.String(500))
-# 	first_name = db.Column(db.String(500))
-# 	last_name = db.Column(db.String(500))
+class user(db.Model):
+	__tablename__ = "user"
+	id = db.Column('id', db.Integer, primary_key = True)
+	user_name = db.Column(db.String(64), index = True, unique = True)
+	email = db.Column(db.String(500), index = True, unique = True)
+	password = db.Column(db.String(500))
+	first_name = db.Column(db.String(500))
+	last_name = db.Column(db.String(500))
+
+	def __repr__(self):
+		return '<User %r>' % (self.user_name)
 
    
